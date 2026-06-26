7aa1648dea5a11684cc481fbd3e55795f2b7003b
Erinnn <EDeJesus671@paps.net>
2026-06-25 22:56:01 -0400

    Delete js/css/js directory

--- js/css/js/js/storage.js
+++
@@ -1,9 +0,0 @@
-const Save = {
-  set(key, value){
-    localStorage.setItem(key, JSON.stringify(value));
-  },
-
-  get(key, fallback){
-    return JSON.parse(localStorage.getItem(key)) ?? fallback;
-  }
-};
